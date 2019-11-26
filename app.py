6daa4d474c58bb7f4f8a02ef6c352ce4607b7404
vladyslavnUA <vladyslavn@yahoo.com>
2019-11-26 03:55:18 -0800

    users can see all playlists

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ def index():
     #homepage
     return render_template('home.html', msg='flask is cool')
 
+# mock array of projects
+playlists = [
+    { 'title': 'cat videos', 'description': 'cats acting weird'},
+    { 'title': '80\'s music', 'description': 'don\'t stop believing!'}
+]
+
+@app.route('/playlists')
+def playlists_index():
+    #index playlists
+    return render_template('playlists_index.html', playlists=playlists)
+    
 if __name__ == '__main__':
     app.run(debug=True)
