6629e5927d565d1f57fa0198bfe1cdc880a59047
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-06 02:39:16 -0800

    added more functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from 
 
 client = MongoClient()
 db = client.Playlister
@@ -38,6 +39,7 @@ def playlists_submit():
         'description': request.form.get('description'),
         'videos': videos,
         'video_ids': video_ids
+        'created_at': datetime.now()
     }
 
     playlists.insert_one(playlist)
@@ -51,6 +53,48 @@ def playlists_submit():
 def playlists_new():
     #create a new playlist
     return render_template('playlists_new.html')
- 
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
+    return render_template('playlists_index.html', playlist=playlist, comments=playlist_comments)
+
+@app.route('/playlists/<playlist_id>', methods=['post'])
+def playlists_update(playlist_id):
+    videos_ids = request.form.get('videos_ids').split()
+    videos = video_url_creator(videos_ids)
+
+    update_playlist = {
+        'title': request.form.get('title'),
+        'descrption': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+@app.route('/playlists/<playlist_id>/delete', methods=['post'])
+def playlists_delete(playlist_id):
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/comments/comments', methods=['post'])
+def comments_new():
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'playlist_id': ObjectId(request.form.get('playlist_id')),
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+
+@app.route('/playlists/comments/<comment_id>', methods=['post'])
+
+
 if __name__ == '__main__':
     app.run(debug=True)
